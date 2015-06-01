ca98d7e2043e965ef46317d3b13fe0916e822a10
vharish <i_v_harish@yahoo.co.in>
2015-06-01 19:07:07 +0530

    Small changes. vimrc to vim, locale defaults in profile.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc zshrc oh-my-zsh tmux.conf my_aliases sh_aliases profile"    # list of files/folders to symlink in homedir
+files="bashrc vim zshrc oh-my-zsh tmux.conf my_aliases sh_aliases profile"    # list of files/folders to symlink in homedir
 
 ##########
 
--- profile
+++ profile
@@ -29,3 +29,7 @@ export EDITOR='vim'
 #else
 #        export TERM='xterm-color'
 #fi
+
+# Setting for the new UTF-8 terminal support in Lion
+LC_CTYPE=en_US.UTF-8
+LC_ALL=en_US.UTF-8
